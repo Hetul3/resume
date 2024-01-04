80196c10ac682c268a93f9562d291a1d3a8e058b
Hetul Patel <patehetu239@gmail.com>
2024-01-04 16:18:38 -0500

    experience section, need to swap prompts

--- pages/api/cohereAPI.js
+++ pages/api/cohereAPI.js
@@ -9,20 +9,23 @@ export default async function handler(req, res) {
     return;
   }
 
-  const { prompts, jobDescription } = body;
+  const { prompts, jobDescription, experience } = body;
 
   if (
     !Array.isArray(prompts) ||
     prompts.length === 0 ||
-    prompts.some((prompt) => !prompt.trim())
+    prompts.some((prompt) => !prompt.trim()) ||
+    !Array.isArray(experience) ||
+    experience.length === 0 ||
+    experience.some((exp) => !exp.trim())
   ) {
     res
       .status(400)
       .json({ error: "Prompts must be provided in a non-empty array" });
     return;
   }
-  if(!jobDescription || typeof jobDescription !== "string") {
-    res.status(400).json({erorr: "Job description not provided as string"});
+  if (!jobDescription || typeof jobDescription !== "string") {
+    res.status(400).json({ erorr: "Job description not provided as string" });
     return;
   }
 
@@ -34,37 +37,88 @@ export default async function handler(req, res) {
 
   try {
     const maxTokensJob = 50;
-    
+
     const generateJob = await cohere.generate({
-        prompt: `Give me the key technologies and skills highlighted from this job description in only one or two words seperated by a comma: "${jobDescription}"`,
-        model: "command-nightly",
-        maxTokens: maxTokensJob,
-        temperature: 0.9,
+      prompt: `Give me the key technologies and skills highlighted from this job description in only one or two words seperated by a comma: "${jobDescription}"`,
+      model: "command-nightly",
+      maxTokens: maxTokensJob,
+      temperature: 0.9,
     });
 
     usedTechnologies = generateJob.generations[0].text;
-  } catch(error) {
+  } catch (error) {
     console.log(error);
-    res.status(500).json({error: "Error when retrieving job description technologies"});
+    res
+      .status(500)
+      .json({ error: "Error when retrieving job description technologies" });
   }
+
+  const result = {
+    prompts: [],
+    experience: [],
+  };
 
   try {
     const maxTokens = 100;
-    const responses = [];
 
-    for (const prompt of prompts) {
+    const promptsGeneration = prompts.map(async (prompt) => {
       const generate = await cohere.generate({
-        prompt: `Use the provided software project description to generate tech resume bullet points for someone showcasing this project: "${prompt.trim()}" with 3 bullet points with a maximum of 15 words each in the style of a tech resume, highlight the key aspects of the project and omit any unnecessary details. Ensure a concise tech resume style, emphasizing the tech stack, user features, and project uniqueness. Highlight with one bullet point what the project is in less than 15 words, with the second bullet point highlight the technologies used in less than 15 words and with the third bullet point highlight something impressive about the project or another technology used in less than 15 words. Make sure to resemble that of a tech resume along with using third person and don't refer to the project. Start with & and a new line, then write bullet points with -, seperate the bullet points with a new line, end the generation with & at the end. Before starting the bullet points, start with an & and a new line, then write all three bullet points before ending with an & and new line. Space each bullet point with a new line. Cater bullet points and information to best fit the technologies highlighted for a job description provided here "${usedTechnologies}", making sure to include the technologies highlighted in the job description if they are in the project. Ensure no speculative data or content that is not explicitly mentioned in the project description is included.`,
+        prompt: `Use the provided software project description to generate tech resume bullet points for someone showcasing this project on their resume: "${prompt.trim()}". Generate 3 bullet points with a maximum of 15 words each in the style of a tech resume, highlight the key aspects of the project and omit any unnecessary details. Ensure a concise tech resume style, emphasizing the tech stack, user features, and project uniqueness. Highlight with one bullet point what the project is in less than 15 words, with the second bullet point highlight the technologies used in less than 15 words and with the third bullet point highlight something impressive about the project or another technology used in less than 15 words. Make sure to resemble the result to that of a tech resume along with using third person and don't refer to the project. Start with ^ and a new line, then write bullet points with -, seperate the bullet points with a new line, end the generation with ^ at the end. Before starting the bullet points, start with an ^ and a new line, then write all three bullet points before ending with an ^ and new line. Space each bullet point with a new line. Cater bullet points and information to best fit the technologies highlighted for a job description provided here: "${usedTechnologies}", making sure to include the technologies highlighted in the job description if they are in the project. Ensure no speculative data or content that is not explicitly mentioned in the project description is never included. The result must be in bullet point.`,
         model: "command-nightly",
         maxTokens: maxTokens,
         temperature: 0.9,
       });
+      return generate;
+    });
 
-      responses.push(generate);
-    }
+    const experienceGeneration = experience.map(async (exp) => {
+        const generate = await cohere.generate({
+            prompt: `Use the provided software experience description to generate tech resume bullet points for someone showcasing this experience on their resume: "${exp.trim()}". Generate 3 bullet points with a maximum of 15 words each in the style of a tech resume, highlight the key aspects of the experience and omit any unnecessary details. Ensure a concise tech resume style, exphasizing the tech stack, user features, and experience uniqueness. Highlight with one bullet point what the experience responsibilities were in less than 15 words, with the second bullet point highlight the technologies used in less than 15 words, and with the third bullet point highlight a softskill showcased in the experience. Make sure to resemble the result to that of a tech resume along with using third person and don't refer to the project. Start with ^ and a new line, then write bullet points with -, seperate the bullet points with a new line, end the generation with ^ at the end. Before starting the bullet points, start with an ^ and a new line, then write all three bullet points before ending with an ^ and new line. Space each bullet point with a new line. Cater bullet points and information to best fit the technologies and skills highlighted for a job description here: "${usedTechnologies}", making sure to include the technologies highlighted in the job description if they are in the experience. Ensure no speculative data or content that is not explicitly mentioned in the experience description is never included. The result must be in bullet point.`,
+            model: "command-nightly",
+            maxTokens: maxTokens,
+            temperature: 0.9,
+          });
+          return generate;
+    });
 
-    res.status(200).json(responses);
+    const [promptResults, experienceResults] = await Promise.all([
+        Promise.all(promptsGeneration),
+        Promise.all(experienceGeneration),
+    ]);
 
+    result.prompts = promptResults;
+    result.experience = experienceResults;
+
+    // for (const prompt of prompts) {
+    //   const generate = await cohere.generate({
+    //     prompt: `Use the provided software project description to generate tech resume bullet points for someone showcasing this project on their resume: "${prompt.trim()}". Generate 3 bullet points with a maximum of 15 words each in the style of a tech resume, highlight the key aspects of the project and omit any unnecessary details. Ensure a concise tech resume style, emphasizing the tech stack, user features, and project uniqueness. Highlight with one bullet point what the project is in less than 15 words, with the second bullet point highlight the technologies used in less than 15 words and with the third bullet point highlight something impressive about the project or another technology used in less than 15 words. Make sure to resemble the result to that of a tech resume along with using third person and don't refer to the project. Start with ^ and a new line, then write bullet points with -, seperate the bullet points with a new line, end the generation with ^ at the end. Before starting the bullet points, start with an ^ and a new line, then write all three bullet points before ending with an ^ and new line. Space each bullet point with a new line. Cater bullet points and information to best fit the technologies highlighted for a job description provided here: "${usedTechnologies}", making sure to include the technologies highlighted in the job description if they are in the project. Ensure no speculative data or content that is not explicitly mentioned in the project description is never included. The result must be in bullet point.`,
+    //     model: "command-nightly",
+    //     maxTokens: maxTokens,
+    //     temperature: 0.9,
+    //   });
+
+    //   responses.push(generate);
+    //   console.log(responses);
+    // }
+
+    // for (const exp of experience) {
+    //   const generate = await cohere.generate({
+    //     prompt: `Use the provided software experience description to generate tech resume bullet points for someone showcasing this experience on their resume: "${exp.trim()}". Generate 3 bullet points with a maximum of 15 words each in the style of a tech resume, highlight the key aspects of the experience and omit any unnecessary details. Ensure a concise tech resume style, exphasizing the tech stack, user features, and experience uniqueness. Highlight with one bullet point what the experience responsibilities were in less than 15 words, with the second bullet point highlight the technologies used in less than 15 words, and with the third bullet point highlight a softskill showcased in the experience. Make sure to resemble the result to that of a tech resume along with using third person and don't refer to the project. Start with ^ and a new line, then write bullet points with -, seperate the bullet points with a new line, end the generation with ^ at the end. Before starting the bullet points, start with an ^ and a new line, then write all three bullet points before ending with an ^ and new line. Space each bullet point with a new line. Cater bullet points and information to best fit the technologies and skills highlighted for a job description here: "${usedTechnologies}", making sure to include the technologies highlighted in the job description if they are in the experience. Ensure no speculative data or content that is not explicitly mentioned in the experience description is never included. The result must be in bullet point.`,
+    //     model: "command-nightly",
+    //     maxTokens: maxTokens,
+    //     temperature: 0.9,
+    //   });
+
+    //   experienceResponse.push(generate);
+    //   console.log(experienceResponse);
+    // }
+
+    // const result = {
+    //   prompts: responses,
+    //   experience: experienceResponse,
+    // };
+
+    res.status(200).json(result);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,41 @@ import { useState } from "react";
 export default function Home() {
   const [inputText, setInputText] = useState([""]);
   const [jobDescription, setJobDescription] = useState("");
+  const [experienceText, setExperienceText] = useState([""]);
   const [generatedText, setGeneratedText] = useState("");
+  const [generatedExperience, setGeneratedExperience] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleAddField = () => {
     setInputText([...inputText, ""]);
   };
 
+  const handleAddExperience = () => {
+    setExperienceText([...experienceText, ""]);
+  };
+
   const handleJobDescriptionChange = (value) => {
     setJobDescription(value);
-  }
+  };
+
+  const handleExperienceChange = (index, value) => {
+    const newExperienceText = [...experienceText];
+    newExperienceText[index] = value;
+    setExperienceText(newExperienceText);
+    console.log(experienceText);
+  };
 
   const handleInputChange = (index, value) => {
     const newInputText = [...inputText];
     newInputText[index] = value;
     setInputText(newInputText);
     console.log(inputText);
+  };
+
+  const handleDeleteExperience = (index) => {
+    const newExperienceText = [...experienceText];
+    newExperienceText.splice(index, 1);
+    setExperienceText(newExperienceText);
   };
 
   const handleDeleteField = (index) => {
@@ -28,7 +47,11 @@ export default function Home() {
   };
 
   const handleSubmit = async () => {
-    if (inputText.some((text) => !text.trim()) || !jobDescription.trim()) {
+    if (
+      inputText.some((text) => !text.trim()) ||
+      !jobDescription.trim() ||
+      experienceText.some((text) => !text.trim())
+    ) {
       setErrorMessage("Fill in all text fields");
       return;
     }
@@ -39,7 +62,11 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ prompts: inputText , jobDescription: jobDescription}),
+        body: JSON.stringify({
+          prompts: inputText,
+          jobDescription: jobDescription,
+          experience: experienceText,
+        }),
       });
 
       if (!response.ok) {
@@ -48,36 +75,70 @@ export default function Home() {
 
       const data = await response.json();
 
-      if (Array.isArray(data) && data.length > 0) {
-        const formattedTextArray = data.map((gen) => {
-          const formattedText = gen?.generations?.[0]?.text || "";
+      console.log(data);
+
+      if (data && data.prompts && data.experience) {
+        const formattedTextArray = [];
+        const formattedExperienceArray = [];
+
+        data.prompts.forEach((item, index) => {
+          const formattedText = item?.generations?.[0]?.text || "";
           const bulletPoints = formattedText
-            .split("&")
+            .split("^")
             .filter((point) => point.trim() !== "");
-      
-          return bulletPoints
-            .map((point) => point.trim())
-            .filter(Boolean) 
-            .join("\n");
+          formattedTextArray.push(
+            bulletPoints
+              .map((point) => point.trim())
+              .filter(Boolean)
+              .join("\n")
+          );
+
+          const formattedExperience =
+            data.experience[index]?.generations?.[0]?.text || "";
+          const expBulletPoints = formattedExperience
+            .split("^")
+            .filter((point) => point.trim() !== "");
+          formattedExperienceArray.push(
+            expBulletPoints
+              .map((point) => point.trim())
+              .filter(Boolean)
+              .join("\n")
+          );
         });
 
         setGeneratedText(formattedTextArray);
+        setGeneratedExperience(formattedExperienceArray);
         setErrorMessage("");
-        console.log(data);
       } else {
+        setGeneratedExperience([]);
         setGeneratedText([]);
-        setErrorMessage("No generated text recieved");
+        setErrorMessage("no generated text recieved");
       }
+      
     } catch (error) {
       console.error(error);
       setGeneratedText("");
+      setGeneratedExperience("");
       setErrorMessage("Error fetching generated text");
     }
   };
   return (
     <div>
       <h1>Generate Text</h1>
-      {/* Existing code for project description */}
+
+      {experienceText.map((value, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => handleExperienceChange(index, e.target.value)}
+            placeholder={`Enter text prompt ${index + 1}`}
+          />
+          <button onClick={() => handleDeleteExperience(index)}>Delete</button>
+        </div>
+      ))}
+      <button onClick={handleAddExperience}>Add Text Field</button>
+
       {inputText.map((value, index) => (
         <div key={index}>
           <input
@@ -91,7 +152,6 @@ export default function Home() {
       ))}
       <button onClick={handleAddField}>Add Text Field</button>
 
-      {/* New field for job description */}
       <div>
         <input
           type="text"
@@ -102,12 +162,22 @@ export default function Home() {
       </div>
 
       <button onClick={handleSubmit}>Submit</button>
-      
+
       {/* Display generated text */}
       {generatedText.length > 0 && (
         <div>
           <h2>Generated Text</h2>
           {generatedText.map((text, index) => (
+            <pre key={index}>{text}</pre>
+          ))}
+        </div>
+      )}
+
+      {/* Display generated experiences */}
+      {generatedExperience.length > 0 && (
+        <div>
+          <h2>Generated Experience</h2>
+          {generatedExperience.map((text, index) => (
             <pre key={index}>{text}</pre>
           ))}
         </div>
